3380f5cb250b06fc32aaca7f1bbe32ba2c0139b8
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-08-01 05:48:35 +0000

    Adding category list into categories

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+      // sidenav initialization
+    let sidnav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, options);
+  });
