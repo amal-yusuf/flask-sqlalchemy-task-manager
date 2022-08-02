efecb41562018295b60caad00bc661e335e3836b
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-08-02 09:45:08 +0000

    Update __init__.py file

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,7 +10,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 db = SQLAlchemy(app)
 
